df9114a2cf4d90fc013e1ad00577d5be041f041f
Yony Brinez <ybrinez@picap.co>
2020-04-25 15:43:52 -0500

    Create models post and comment, include validations, configure annotate gem for view fields of models

---
+++ app/models/comment.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :integer
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
+#
+class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :user
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+end
---
+++ app/models/post.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 250 }
+  validates :user_id, presence: true
+end
